143b6e6a45222db56b28aaedaf7779ce391b67ca
Alberto <showmaster@live.it>
2020-09-07 11:23:06 +0200

    Prova continuos integration

--- src/main/java/myjenkinsjob/MyJenkinsJob.java
+++ src/main/java/myjenkinsjob/MyJenkinsJob.java
@@ -3,5 +3,6 @@ package myjenkinsjob;
 public class MyJenkinsJob {
     public static void main(String[] args) {
         System.out.println("Primo progetto Jenkins!!");
+        System.out.println("Proba continuos integration..");
     }
 }
